e361f9a4582a9cab9087a88a61cd385b720c9d4d
Ezugworie Ifeanyi Ofor <ofotronics.2010@gmail.com>
2021-12-06 02:31:11 +0100

    Write tests for the search feature before implementing search

--- 1_Requests_Starter/backend/flaskr_test.py
+++ 1_Requests_Starter/backend/flaskr_test.py
@@ -25,9 +25,9 @@ class BookTestCase(unittest.TestCase):
         setup_db(self.app, self.database_path)
 
         self.new_book = {
-            "title": "A Day In Chicago",
-            "author": "Ezugworie Eucharia",
-            "rating": "4"
+            "title": "The Article",
+            "author": "Uche M. N",
+            "rating": "5"
         }
 
         # binds the app to the current context
@@ -63,9 +63,30 @@ class BookTestCase(unittest.TestCase):
         self.assertTrue(data["total_books"])
         self.assertTrue(len(data["books"]))
 
+    def test_successful_search_found_book(self):
+        '''Creating a successful book-search test before implementing search feature'''
+        resp = self.client().post('/books', json = {"search": "Code"})
+        data = json.loads(resp.data)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_books'])
+        self.assertTrue(len(data['books']))
+        self.assertEqual(len(data['books']), 3)
+
+    def test_successful_search_no_book(self):
+        '''Creating a book_not_found search test before implementing search feature'''
+        resp = self.client().post('/books', json={"search": "Ezechitoke"})
+        data = json.loads(resp.data)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(data["success"], True)
+        self.assertEqual(data["total_books"], 0)
+        self.assertEqual(len(data["books"]), 0)
+
     def test_edit_book_rating(self):
         '''Testing to see if book rating is edited as expected'''
-        resp = self.client().patch("/books/17", json = {"rating": "5"})
+        resp = self.client().patch("/books/34", json = {"rating": "5"})
         data = json.loads(resp.data)
 
         self.assertEqual(resp.status_code, 200)
@@ -74,15 +95,15 @@ class BookTestCase(unittest.TestCase):
 
     def test_delete_book(self):
         '''Testing to see if books are deleted as expected'''
-        resp = self.client().delete('/books/16')
+        resp = self.client().delete('/books/39')
         data = json.loads(resp.data)
 
-        book = Book.query.filter(Book.id == 16).one_or_none()
+        book = Book.query.filter(Book.id == 39).one_or_none()
 
         self.assertEqual(resp.status_code, 200)
         self.assertEqual(data['success'], True)
         self.assertTrue(data["deleted"])
-        self.assertEqual(data['deleted'], 16)
+        self.assertEqual(data['deleted'], 39)
         self.assertTrue(data["total_books"])
         self.assertTrue(len(data["books"]))
 
@@ -96,8 +117,7 @@ class BookTestCase(unittest.TestCase):
 
         self.assertEqual(resp.status_code, 400)
         self.assertEqual(data['success'], False)
-        self.assertEqual(
-            data['message'], "The server could not understand the request due to invalid syntax")
+        self.assertEqual(data['message'], "The server could not understand the request due to invalid syntax")
 
 
     def test_list_books_beyond_valid_page(self):
@@ -107,8 +127,7 @@ class BookTestCase(unittest.TestCase):
 
         self.assertEqual(resp.status_code, 404)
         self.assertEqual(data['success'], False)
-        self.assertEqual(
-            data['message'], "The server can not find the requested resource")
+        self.assertEqual(data['message'], "The server can not find the requested resource")
     
 
     def test_not_allowed_method(self):
@@ -118,8 +137,7 @@ class BookTestCase(unittest.TestCase):
 
         self.assertEqual(resp.status_code, 405)
         self.assertEqual(data['success'], False)
-        self.assertEqual(
-            data['message'], "This method is not allowed for the requested URL")
+        self.assertEqual(data['message'], "This method is not allowed for the requested URL")
 
 
     def test_delete_nonexisting_book(self):
@@ -129,8 +147,7 @@ class BookTestCase(unittest.TestCase):
 
         self.assertEqual(resp.status_code, 422)
         self.assertEqual(data['success'], False)
-        self.assertEqual(
-            data['message'], "The request was unable to be followed due to semantic errors")
+        self.assertEqual(data['message'], "The request was unable to be followed due to semantic errors")
 
 
 
