3c4a0c925d7efe2cd725e0f182bce18b9b2b1ea5
Ezugworie Ifeanyi Ofor <ofotronics.2010@gmail.com>
2021-12-08 13:02:21 +0100

    Edit database path

--- 1_Requests_Starter/backend/flaskr_test.py
+++ 1_Requests_Starter/backend/flaskr_test.py
@@ -20,7 +20,7 @@ class BookTestCase(unittest.TestCase):
         self.client = self.app.test_client
         self.database_name = "bookshelf_test"
         self.database_path = "postgresql://{}:{}@{}/{}".format(
-            "ezugworie", "E2u8w0r1e", "localhost:5432", self.database_name
+            "student", "student", "localhost:5432", self.database_name
         )
         setup_db(self.app, self.database_path)
 
